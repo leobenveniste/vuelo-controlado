3c33d21a0182fc65af32192accf70851770c4075
Leonardo Benveniste <leonardo.benveniste@guardline.io>
2026-04-30 11:18:15 -0300

    Fix: User adjustment of donation iframe scroll position to -1080px.

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -60,7 +60,7 @@ const DonatePage: React.FC = () => {
              src={DONATE_URL}
              width="100%" 
              height="2000px" 
-             style={{ marginTop: '-1050px' }}
+             style={{ marginTop: '-1080px' }}
              frameBorder="0" 
              allowTransparency={true}
              className="w-full relative z-10"
